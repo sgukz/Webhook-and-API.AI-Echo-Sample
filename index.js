15af0b10258400a61c747ffcd68e6b96ba2f9f88
sgukz <thorapee055@gmail.com>
2019-05-30 14:34:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ restService.post("/echo", function(req, res) {
   return res.json({
     payload: temp,
     data: temp,
-    fulfillmentText: "Sample response",
+    fulfillmentText: speech,
     speech: speech,
     displayText: speech,
     source: "webhook-echo-sample"
