006b7314ecaa3f99c15e324f2a5b5caeb922c1e0
sgukz <thorapee055@gmail.com>
2019-06-04 16:31:08 +0700

    update

--- index.js
+++ index.js
@@ -19,17 +19,22 @@ restService.post("/webhook", function (req, res) {
       req.body.queryResult.parameters &&
       req.body.queryResult.parameters.height &&
       req.body.queryResult.parameters.weight
-      ? "ส่วนสูง : "+req.body.queryResult.parameters.height +"\nน้ำหนัก: "+req.body.queryResult.parameters.weight
-      // ? JSON.stringify(req.body)
+      //? "ส่วนสูง : "+req.body.queryResult.parameters.height +"\nน้ำหนัก: "+req.body.queryResult.parameters.weight
+      ? JSON.stringify(req.body)
       : "try again.";
+
   return res.json({
-    fulfillmentMessages: [{ 
-      "type": "text",
-      "text": speech, 
-      "platform": "LINE" 
-    }],
+    fulfillmentText: speech,
     source: "line"
   });
+  // return res.json({
+  //   fulfillmentMessages: [{ 
+  //     "type": "text",
+  //     "text": speech, 
+  //     "platform": "LINE" 
+  //   }],
+  //   source: "line"
+  // });
 });
 
 restService.listen(process.env.PORT || 8000, function () {
