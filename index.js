3a8b9754844708d56881fbf18e87ca2a126e117f
sgukz <thorapee055@gmail.com>
2019-06-04 15:37:47 +0700

    update push now

--- index.js
+++ index.js
@@ -1,69 +1,32 @@
+"use strict";
 
-'use strict';
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const functions = require('firebase-functions');
-const { WebhookClient } = require('dialogflow-fulfillment');
-const { Card, Suggestion, Payload } = require('dialogflow-fulfillment');
+const restService = express();
 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+restService.use(bodyParser.json());
 
-  function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
-  }
+restService.post("/webhook", function (req, res) {
+  var speech =
+    req.body.queryResult &&
+      req.body.queryResult.parameters &&
+      req.body.queryResult.parameters.height &&
+      req.body.queryResult.parameters.weight
+      ? req.body.queryResult.parameters.height + " " + req.body.queryResult.parameters.weight
+      : "try again.";
+  return res.json({
+    fulfillmentText: speech,
+    source: "webhook-echo-sample"
+  });
+});
 
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-  }
-
-  function rehRightsIndex(agent) {
-    let height = request.body.queryResult.parameters.height / 100;
-    let weight = request.body.queryResult.parameters.weight;
-    let bmi = (weight / (height * height)).toFixed(2);
-
-    let payloadJson = {
-
-      "type": "flex",
-      "altText": "ผลการลงทะเบียน",
-      "contents": {
-        "type": "bubble",
-        "header": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "ลงทะเบียนเรียบร้อยแล้ว",
-              "size": "xs",
-              "weight": "bold",
-              "color": "#17c950"
-            }
-          ]
-        },
-        "body": {
-          "type": "box",
-          "layout": "horizontal",
-          "contents": [
-            {
-              "type": "text",
-              "text": bmi,
-              "align": "center"
-            }
-          ]
-        }
-      }
-    };
-    let payload = new Payload("LINE", payloadJson, { sendAsMessage: true });
-    agent.add(payload);
-  }
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
-  intentMap.set('REH Rights - custom - yes', rehRightsIndex);
-  agent.handleRequest(intentMap);
+restService.listen(process.env.PORT || 8000, function () {
+  console.log("Server up and listening");
 });
