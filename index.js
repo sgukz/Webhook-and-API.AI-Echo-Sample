2cf8a4b016fadf68f1319b868da44526f9eb9c98
sgukz <thorapee055@gmail.com>
2019-06-05 11:09:12 +0700

    update

--- index.js
+++ index.js
@@ -14,12 +14,60 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/webhook", function (req, res) {
-  let height = req.body.queryResult.parameters.height/100;
+  let height = req.body.queryResult.parameters.height / 100;
   let weight = req.body.queryResult.parameters.weight;
-  let bmi = (weight/ (height * height)).toFixed(2);
+  let bmi = (weight / (height * height)).toFixed(2);
 
   return res.json({
-    fulfillmentText: JSON.stringify(req.body),
+    //fulfillmentText: JSON.stringify(req.body),
+    fulfillmentMessages: [{
+      "payload": {
+        "line": {
+          "type": "flex",
+          "altText": "ค่าดัชนีมวลกาย (BMI)",
+          "contents": {
+            "type": "bubble",
+            "styles": {
+              "header": {
+                "backgroundColor": "#1e81e7"
+              }
+            },
+            "header": {
+              "type": "box",
+              "layout": "baseline",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": " ค่าดัชนีมวลกาย (BMI)",
+                  "weight": "bold",
+                  "size": "md",
+                  "gravity": "top",
+                  "color": "#FFFFFF",
+                  "flex": 0
+                }
+              ]
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "bmi",
+                  "weight": "bold",
+                  "size": "xl"
+                },
+                {
+                  "type": "text",
+                  "text": "อ้วน"
+                }
+              ]
+            }
+          }
+        }
+      },
+      "platform": "LINE"
+    }],
     source: "line"
   });
 });
